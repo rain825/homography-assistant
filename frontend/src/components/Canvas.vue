d5d9ae9364ef9ca30ffe7818bf5bb91a5ec5dbe0
rain9525 <rain9525@users.noreply.github.com>
2019-11-23 11:40:24 +0900

    draw canvas

--- frontend/src/components/Canvas.vue
+++ frontend/src/components/Canvas.vue
@@ -5,9 +5,13 @@
 <script>
 export default {
   props: {
-    radius: {
+    canvasWidth: {
       type: Number,
-      default: 50
+      default: 500
+    },
+    canvasHeight: {
+      type: Number,
+      default: 500
     }
   },
   mounted() {
@@ -27,12 +31,57 @@ export default {
       // 一番目のファイルを取得
       let file = e.dataTransfer.files[0];
       console.log(file.name);
+
+      // ファイルタイプチェック
+      if (!file.type.match("image.*")) {
+        console.log("not image file");
+        return;
+      }
+
+      let ctx = this.ctx;
+      let canvasWidth = this.canvasWidth;
+      let canvasHeight = this.canvasHeight;
+
+      // ファイル読み込み
+      let fileReader = new FileReader();
+      // 読み込み完了時にキャンバス描画
+      fileReader.onload = function(e) {
+        let img = new Image();
+        img.onload = function() {
+          // 描画サイズを計算
+          let drawCanvasWidth, drawCanvasHeight;
+          if (img.naturalWidth > img.naturalHeight) {
+            drawCanvasWidth = canvasWidth;
+            drawCanvasHeight =
+              (img.naturalHeight / img.naturalWidth) * canvasWidth;
+          } else {
+            drawCanvasWidth =
+              (img.naturalWidth / img.naturalHeight) * canvasHeight;
+            drawCanvasHeight = canvasHeight;
+          }
+          let x =
+            (canvasWidth - drawCanvasWidth) / 2 > 0
+              ? (canvasWidth - drawCanvasWidth) / 2
+              : 0;
+          let y =
+            (canvasHeight - drawCanvasHeight) / 2 > 0
+              ? (canvasHeight - drawCanvasHeight) / 2
+              : 0;
+
+          // 描画
+          ctx.drawImage(img, x, y, drawCanvasWidth, drawCanvasHeight);
+        };
+
+        img.src = e.target.result;
+      };
+
+      fileReader.readAsDataURL(file);
     },
     // ドラッグ時の処理
     dragOver(e) {
       e.stopPropagation();
       e.preventDefault();
-      e.dataTransfer.dropEffect = "draw"; // Explicitly show this is a copy.
+      e.dataTransfer.dropEffect = "copy";
     }
   }
 };
