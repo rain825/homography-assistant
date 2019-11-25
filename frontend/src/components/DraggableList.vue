ac86de92d969b1a1215cfb63d168269a41e89436
popunbom <fantom0779@gmail.com>
2019-11-25 13:26:49 +0900

    Implement UI (finish) - ひととおり完成

--- frontend/src/components/DraggableList.vue
+++ frontend/src/components/DraggableList.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="wrapper">
     <draggable v-model="listItem" @start="drag = true" @end="drag = false">
-      <div class="item" v-for="(item, index) in listItem" :key="item">
-        {{ index }}: {{ item }}
+      <div class="item-wrapper" v-for="(item, index) in listItem" :key="index">
+        <div
+          :style="{ 'background-color': item.color }"
+          class="item-color"
+        ></div>
+        ( {{ item.pos.x }}, {{ item.pos.y }} )
       </div>
     </draggable>
   </div>
@@ -11,6 +15,10 @@
 <script>
 import draggable from "vuedraggable"
 
+Array.prototype.choice = function() {
+  return this[Math.floor(Math.random() * this.length)]
+}
+
 export default {
   name: "Test",
   components: {
@@ -18,10 +26,32 @@ export default {
   },
   data() {
     return {
-      listItem: ["チノ", "ココア", "リゼ", "シャロ", "千夜"],
+      colors: [
+        "cyan",
+        "blue",
+        "magenta",
+        "maroon",
+        "lime",
+        "red",
+        "yellow",
+        "green",
+        "navy",
+        "orange",
+      ],
+      listItem: new Array(),
     }
   },
-  created() {},
+  created() {
+    for (let i = 0; i < 5; i++) {
+      this.listItem.push({
+        pos: {
+          x: parseInt(Math.random() * 1000, 10),
+          y: parseInt(Math.random() * 1000, 10),
+        },
+        color: this.colors.choice(),
+      })
+    }
+  },
   methods: {},
 }
 </script>
@@ -31,7 +61,7 @@ export default {
   width: 300px;
   border: 3px solid blue;
 }
-.item {
+.item-wrapper {
   font-size: 2em;
   padding-left: 1ex;
   margin: 10px;
@@ -39,9 +69,22 @@ export default {
   font-weight: bold;
   border: 3px solid rgba(0, 0, 0, 0.3);
   box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.2);
+
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
 }
 
-.item:hover {
+.item-wrapper:hover {
   background-color: rgba(64, 128, 255, 0.3);
+}
+
+.item-color {
+  width: 25px;
+  height: 25px;
+  margin-right: 8px;
+  border: 3px solid black;
+  border-radius: 50%;
 }
 </style>
