b661fba6e8682196b43c8b574f10f368e2c7fb33
popunbom <fantom0779@gmail.com>
2019-11-26 13:53:20 +0900

    Fix: validator.js - points.every の返却値がおかしかったので修正

--- frontend/src/utils/validator.js
+++ frontend/src/utils/validator.js
@@ -1,8 +1,6 @@
 export function pointsValidator(points) {
   return (
     points.length == 0 ||
-    points.every(point => {
-      "color" in point && "pos" in point
-    })
+    points.every(point => "color" in point && "pos" in point)
   )
 }
