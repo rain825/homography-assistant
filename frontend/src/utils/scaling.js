51139082fdc266c9c61400412c0e61daa82d5ce3
popunbom <fantom0779@gmail.com>
2019-11-30 18:33:25 +0900

    Edit: scaling.js - calcScale 関数を ImageCanvas から移動

--- frontend/src/utils/scaling.js
+++ frontend/src/utils/scaling.js
@@ -1,48 +1,62 @@
-export const calcScale = {
-  basedOnWidth(width, image) {
-    const ratio = width / image.naturalWidth
-    console.log(`Ratio: ${ratio}`)
-    return {
-      ratio: ratio,
-      size: {
-        image: {
-          native: {
-            width: image.naturalWidth,
-            height: image.naturalHeight,
-          },
-          scaled: {
-            x: image.naturalWidth * ratio,
-            y: image.naturalHeight * ratio,
-          },
+function basedOnWidth(width, image) {
+  const ratio = width / image.naturalWidth
+  console.log(`Ratio: ${ratio}`)
+  return {
+    ratio: ratio,
+    size: {
+      image: {
+        native: {
+          width: image.naturalWidth,
+          height: image.naturalHeight,
         },
-        canvas: {
-          width: width,
+        scaled: {
+          width: image.naturalWidth * ratio,
           height: image.naturalHeight * ratio,
         },
       },
-    }
-  },
-  basedOnHeight(height, image) {
-    const ratio = height / image.naturalHeight
-    console.log(`Ratio: ${ratio}`)
-    return {
-      ratio: ratio,
-      size: {
-        image: {
-          native: {
-            width: image.naturalWidth,
-            height: image.naturalHeight,
-          },
-          scaled: {
-            x: image.naturalWidth * ratio,
-            y: image.naturalHeight * ratio,
-          },
+      canvas: {
+        width: width,
+        height: image.naturalHeight * ratio,
+      },
+    },
+  }
+}
+function basedOnHeight(height, image) {
+  const ratio = height / image.naturalHeight
+  console.log(`Ratio: ${ratio}`)
+  return {
+    ratio: ratio,
+    size: {
+      image: {
+        native: {
+          width: image.naturalWidth,
+          height: image.naturalHeight,
         },
-        canvas: {
+        scaled: {
           width: image.naturalWidth * ratio,
-          height: height,
+          height: image.naturalHeight * ratio,
         },
       },
+      canvas: {
+        width: image.naturalWidth * ratio,
+        height: height,
+      },
+    },
+  }
+}
+
+export function calcScaling({ width, height, image } = {}) {
+  if (width !== undefined && height !== undefined) {
+    if (width >= height) {
+      return basedOnWidth(width, image)
+    } else {
+      return basedOnHeight(height, image)
     }
-  },
+  } else {
+    if (width !== undefined) {
+      return basedOnWidth(width, image)
+    } else {
+      return basedOnHeight(height, image)
+    }
+  }
 }
