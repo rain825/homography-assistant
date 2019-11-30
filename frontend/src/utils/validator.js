59461a626ef5485c04a0ef6da235929701efe7e3
popunbom <fantom0779@gmail.com>
2019-11-30 18:33:30 +0900

    Edit: validator.js - calcScaling の返却値に関するバリデータを追加

--- frontend/src/utils/validator.js
+++ frontend/src/utils/validator.js
@@ -4,3 +4,17 @@ export function pointsValidator(points) {
     points.every(point => "id" in point && "color" in point && "pos" in point)
   )
 }
+
+export function scalingValidator(scaling) {
+  return (
+    ["ratio", "size"].every(key => key in scaling) &&
+    ["image", "canvas"].every(key => key in scaling.size) &&
+    ["native", "scaled"].every(key => key in scaling.size.image) &&
+    ["width", "height"].every(key => key in scaling.size.image.native) &&
+    [
+      scaling.size.image.native,
+      scaling.size.image.scaled,
+      scaling.size.canvas,
+    ].every(object => ["width", "height"].every(key => key in object))
+  )
+}
