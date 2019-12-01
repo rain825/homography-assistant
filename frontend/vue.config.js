1c7e706e75c7db0917252dcdc3e38c0248cd08ad
popunbom <fantom0779@gmail.com>
2019-12-01 13:06:57 +0900

    Edit: vue.config.js - vue-svg-loader の設定を追加

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,6 +3,13 @@ const path = require("path")
 module.exports = {
   outputDir: path.resolve(__dirname, "../backend/dist"),
   assetsDir: "static",
+  chainWebpack: config => {
+    const svgRule = config.module.rule("svg")
+
+    svgRule.uses.clear()
+
+    svgRule.use("vue-svg-loader").loader("vue-svg-loader")
+  },
   devServer: {
     port: 8080,
     proxy: {
