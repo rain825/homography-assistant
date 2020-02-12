3640fb24891251ebe8065f0670cad15579a24602
popunbom <fantom0779@gmail.com>
2020-02-12 16:08:35 +0900

    Add template HTML

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,7 @@ const path = require("path")
 
 module.exports = {
   outputDir: path.resolve(__dirname, "../backend/dist"),
+  indexPath: path.resolve(__dirname, "./src/index.html"),
   assetsDir: "static",
   chainWebpack: config => {
     const svgRule = config.module.rule("svg")
@@ -9,6 +10,14 @@ module.exports = {
     svgRule.uses.clear()
 
     svgRule.use("vue-svg-loader").loader("vue-svg-loader")
+  },
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "public/index.html",
+      filename: "index.html",
+      title: "homography-assistant",
+    },
   },
   devServer: {
     port: 8080,
