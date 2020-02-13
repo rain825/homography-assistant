ea2d3bd1f8f8329940a63ead4b0794ae4ade771d
popunbom <fantom0779@gmail.com>
2020-02-13 11:59:48 +0900

    Edit: vuetify.js - カラーテーマを設定

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -2,6 +2,8 @@ import Vue from "vue"
 import Vuetify from "vuetify/lib"
 import ja from "vuetify/es5/locale/ja"
 
+import colors from "vuetify/lib/util/colors"
+
 Vue.use(Vuetify)
 
 export default new Vuetify({
@@ -11,13 +13,22 @@ export default new Vuetify({
     },
     themes: {
       light: {
-        primary: "#ee44aa",
-        secondary: "#424242",
-        accent: "#82B1FF",
-        error: "#FF5252",
-        info: "#2196F3",
-        success: "#4CAF50",
-        warning: "#FFC107",
+        primary: colors.teal.lighten4,
+        secondary: colors.blueGrey.lighten4,
+        accent: colors.lightBlue.lighten4,
+        error: colors.red.lighten4,
+        warning: colors.orange.lighten4,
+        info: colors.brown.lighten4,
+        success: colors.green.lighten4,
+      },
+      dark: {
+        primary: colors.teal.base,
+        secondary: colors.blueGrey.base,
+        accent: colors.lightBlue.base,
+        error: colors.red.base,
+        warning: colors.orange.base,
+        info: colors.brown.base,
+        success: colors.green.base,
       },
     },
   },
